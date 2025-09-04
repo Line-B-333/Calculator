9b1f980a5a848fbf5221f46f7909e2e07817ae6c
Line-B-333 <10187764@ucn.dk>
2025-09-04 19:47:59 +0200

    Added: Program, a short version of using the calculator

--- Calculator.CommandLineApp/Program.cs
+++ Calculator.CommandLineApp/Program.cs
@@ -8,7 +8,42 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        CalcLib.Calculator calculator = new CalcLib.Calculator();
+        CalcLib.Calculator.Calculation(args);
+
+        /*
+        if (args.Length >= 3)
+        {
+            float firstNumber = float.Parse(args[0]);
+            string operation = args[1];
+            float secondNumber = float.Parse(args[2]);
+            
+
+            if (operation == "+")
+            {
+                float result = CalcLib.Calculator.Add(firstNumber, secondNumber);
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+            }
+            else if (operation == "-")
+            {
+                float result = CalcLib.Calculator.Subtract(firstNumber, secondNumber);
+                Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
+            }
+            else if (operation == "/")
+            {
+                float result = CalcLib.Calculator.Divide(firstNumber, secondNumber);
+                Console.WriteLine($"{firstNumber} / {secondNumber} = {result}");
+            }
+            else if (operation == "*")
+            {
+                float result = CalcLib.Calculator.Multiply(firstNumber, secondNumber);
+                Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+            }
+                       
+        }
+        else
+        {
+            Console.WriteLine("\"Unsupported operation. Please use '+', '-', '/' or '*' \ne.g 2.1 + 3.5.");
+        }*/
+   
     }
 }
